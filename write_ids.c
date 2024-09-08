1287873b74d2990ef1e1199da3bd98ae1d1f37ed
Joseph DeVito <109080835+JoeyDeVito@users.noreply.github.com>
2024-09-08 12:08:13 -0400

    Update write_ids.c

--- write_ids.c
+++ write_ids.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 int main() {
-    char GITHUB_ID[] = "Fill_in_Your_Github_Id";
-    char STUDENT_ID[] = "Fill_in_Your_Student_Id";
+    char GITHUB_ID[] = "JoeyDeVito";
+    char STUDENT_ID[] = "948936052";
 
     return 0 ;
 }
